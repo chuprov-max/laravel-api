1e0f649cf34b03687a6f5640b605bf766c681df5
Maxim Chuprov <chuprov.maxim@gmail.com>
2020-05-29 16:09:30 +0300

    Integration With Yandex SpeechKit API

--- app/Article.php
+++ app/Article.php
@@ -7,6 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     const STATUS_CREATED = 1;
-    const STATUS_PREPARED_FOR_REQUEST_TO_YANDEX = 2;
+    const STATUS_QUEUED = 2;
     const STATUS_SEND_REQUEST_TO_YANDEX = 3;
+    const STATUS_SUCCESS_SYNTHESIZE_SPEECH = 4;
+    const STATUS_FAILED_SYNTHESIZE_SPEECH = 5;
+
+    /**
+     * @param bool $checkExists
+     * @return string|null
+     */
+    public function getSpeechFileStoragePath(bool $checkExists = false)
+    {
+        $filePath = storage_path("app/speech/{$this->id}.ogg");
+        if (!$checkExists) {
+            return $filePath;
+        }
+
+        if (file_exists($filePath)) {
+            return $filePath;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param int $status
+     * @return bool
+     */
+    public function changeStatus(int $status)
+    {
+        $this->status = $status;
+
+        return $this->save();
+    }
 }
